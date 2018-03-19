d900010018c2c751da23b7409fffa0311b31f2ca
Nadezhda Vidman <delfinka1984@gmail.com>
2018-03-20 00:18:31 +0200

    Unit4. Task 8. Litecart -> Checking that all ducks have the stickers

---
+++ java-example/src/test/java/ru/stqa/training/selenium/Unit4/Task_8_litecart_stickers.java
@@ -0,0 +1,41 @@
+package ru.stqa.training.selenium.Unit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
+
+
+public class Task_8_litecart_stickers extends TestBase {
+
+    @Test
+    public void litecartDucksAndStickers() {
+        driver.get("http://localhost/litecart/en/");
+
+        //One of ducks is present
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.image-wrapper")));
+
+        //To choose all ducks on the page
+        List<WebElement> listOfDucks = driver.findElements(By.cssSelector("div.image-wrapper"));
+        //Count of all ducks on the page
+        int countOfDucks = listOfDucks.size();
+
+        //To choose all stickers "New" on the page
+        List<WebElement> listOfStickersNew = driver.findElements(By.cssSelector("div.sticker.new"));
+        int countOfStickersNew = listOfStickersNew.size();
+
+        //To choose all stickers "Sale" on the page
+        List<WebElement> listOfStickerSale = driver.findElements(By.cssSelector("div.sticker.sale"));
+        int countOfStickerSale = listOfStickerSale.size();
+
+        //Amount of all "New" and "Sale" stickers on the page
+        int countOfStickers = countOfStickersNew+countOfStickerSale;
+
+        //If count of ducks = count of stickers - then all ducks have a stickers
+        Assert.assertTrue("Not all ducks have a stickers!",countOfDucks == countOfStickers);
+
+    }
+}
--- java-example/src/test/java/ru/stqa/training/selenium/Unit4/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/Unit4/TestBase.java
@@ -30,7 +30,7 @@ public class TestBase {
 
     @After
     public void stop() {
-        driver.quit();
-        driver = null;
+        //driver.quit();
+        //driver = null;
     }
 }
