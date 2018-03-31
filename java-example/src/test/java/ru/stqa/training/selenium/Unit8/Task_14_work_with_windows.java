5359fd208b673f8a007bd99164ee7581a056e78b
Nadezhda Vidman <delfinka1984@gmail.com>
2018-03-31 22:49:03 +0300

    Unit8|Task 14 - work with the additional windows

--- java-example/src/test/java/ru/stqa/training/selenium/Unit8/Task_14_work_with_windows.java
+++ java-example/src/test/java/ru/stqa/training/selenium/Unit8/Task_14_work_with_windows.java
@@ -11,26 +11,18 @@ import java.util.Set;
 
 public class Task_14_work_with_windows extends TestBase {
 
-
     public static ExpectedCondition<String> newWindowHandleIs(
             final Set<String> existingWindows) {
         return new ExpectedCondition<String>() {
             public String apply(WebDriver driver) {
-                String newWindowHandle = "";
-                do{
-                Set<String> Windows = driver.getWindowHandles();
-                    System.out.println("test wind all= "+existingWindows);
-                    System.out.println("test new wins= "+Windows);
-                Windows.removeAll(existingWindows);
-                newWindowHandle = Windows.toString();
-                    System.out.println("test cond= "+newWindowHandle);
-                }
-                while (newWindowHandle.equals(""));
-                return newWindowHandle;
-                }
+
+
+                Set<String> handles = driver.getWindowHandles();
+                handles.removeAll(existingWindows);
+                return handles.size() > 0 ? handles.iterator().next() : null;
+            }
         };
     }
-
 
     @Test
     public void testWindows(){
@@ -42,45 +34,25 @@ public class Task_14_work_with_windows extends TestBase {
 
         //Left Menu -> Countries
         driver.findElement(By.cssSelector("i.fa.fa-flag.fa-stack-1x.icon")).click();
-
         driver.findElement(By.cssSelector("a[title=Edit]")).click();
 
-        List<WebElement> externalLinks = driver.findElements(By.cssSelector("i.fa.fa-external-link"));
+        List<WebElement> externalLinks = driver.findElements(By.cssSelector("i.fa.fa-external-link")); //список вебэлементов ведущих на доп. окна
 
         int linksCount = externalLinks.size();
         for (int i = 0; i < linksCount; i++) {
-            List<WebElement> Links = driver.findElements(By.cssSelector("i.fa.fa-external-link")); // получаем снова список вебэлементов
+            System.out.println("number of iteration = "+(i+1));
+            List<WebElement> Links = driver.findElements(By.cssSelector("i.fa.fa-external-link")); //список вебэлементов ведущих на доп. окна
             WebElement linkExternal = Links.get(i);
-  String aaa = driver.getCurrentUrl();
 
-            System.out.println("a1="+aaa);
             String mainWindow = driver.getWindowHandle();// запоминаем идентификатор текущего окна
-            System.out.println("test old win= "+ mainWindow);
             Set<String> allWindows = driver.getWindowHandles(); // запоминаем идентификаторы уже открытых окон
             linkExternal.click(); // открывает новое окно
 
             String newWindow = wait.until(newWindowHandleIs(allWindows)); // ждем появления нового окна, с новым идентификатором
 
-            System.out.println("fhgdfhfjdfhjdh="+driver.getWindowHandles());
-
-            System.out.println("test 1");
-            String a1 = driver.getCurrentUrl();
-            System.out.println("a2="+a1);
-            if ( !mainWindow.equals(newWindow)){
-
-                System.out.println("new in main = "+newWindow);
-                driver.switchTo().window(newWindow);
-                System.out.println("a new = "+driver.getCurrentUrl());
-
-            }
-
-
-            System.out.println("test 2");
-            //driver.close();
-            System.out.println("test 3");
-            driver.switchTo().window(mainWindow);
-            System.out.println("test 4");
-            driver.navigate().back();
+            driver.switchTo().window(newWindow); // переключаемся в новое окно
+            driver.close(); // закрываем новое окно
+            driver.switchTo().window(mainWindow); // возвращаемся в главное окно
             }
         }
 }
